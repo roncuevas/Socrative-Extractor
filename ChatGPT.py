c0e5c1b015e090e5710882ce50e7e227b2bfabb3
Aarón Cuevas <github@roncuevas.com>
2023-10-01 18:44:40 -0600

    fix: Remove chatgpt models call

--- ChatGPT.py
+++ ChatGPT.py
@@ -3,7 +3,6 @@ import openai
 
 def get_response(api_key, question):
     openai.api_key = api_key
-    openai.Model.list()
     completion = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=[
